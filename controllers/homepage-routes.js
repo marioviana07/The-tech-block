b3a7c0f9bcc3481ec798e387df242b2489bdb801
marioviana07 <mario_viana_2005@yahoo.com>
2020-11-01 09:15:12 -0800

    Creating controllers

---
+++ controllers/homepage-routes.js
@@ -0,0 +1,62 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const { response } = require("express");
+
+router.get("/", (req, res) => {
+    Post.findAll({
+        include: [User],
+
+    })
+    .then((dbPostData) => {
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+        res.render("all-posts", { posts });
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get("/post/:id", (req, res) => {
+    Post.findByPk(req.params.id, {
+        include: [User,
+        {
+            model: Comment,
+            include: [User]
+        }],
+
+    })
+    .then((dbPostData) => {
+        if (dbPostData){
+        const posts = dbPostData.get({plain: true });
+        res.render("single-post", { posts }); 
+    }else{
+        res.status(404).end();
+    }
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+    res.render("login");
+});
+
+router.get("/singup", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+    res.render("singup");
+    
+});
+
+
+module.exports = router;
