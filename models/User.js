0ea66dc99b911c96e441c65fe729e8108c18618f
marioviana07 <mario_viana_2005@yahoo.com>
2020-11-02 13:04:00 -0800

    creating routes files

--- models/User.js
+++ models/User.js
@@ -58,18 +58,3 @@ User.init({
 });
 
 module.exports = User;
-
-
-
-
-// // define an email column
-    // email: {
-    //     type: DataTypes.STRING,
-    //     allowNull: false,
-    //     // there cannot be any duplicate email values in this table
-    //     unique: true,
-    //     // if allowNull is set to false, we can run our data through validators before creating the table data
-    //     validate: {
-    //         isEmail: true
-    //     }
-    // },
