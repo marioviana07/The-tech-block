5b8dc967a7b077f087e0844ae62e74ac3e3aeb28
marioviana07 <mario_viana_2005@yahoo.com>
2020-11-03 17:27:08 -0800

    api user-route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -11,9 +11,78 @@ router.post('/', (req, res) => {
         .then(dbUserData => {
             req.session.save(() => {
                 req.session.userId = dbUserData.id;
-            })
-        .catch(err => {
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
+        .catch((err) => {
             console.log(err);
             res.status(500).json(err);
         });
 });
+
+router.post("/login", (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username,
+        },
+    })
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "No user found" });
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: "The password you enter is Incorrect"});
+            return;
+        }
+        req.session.save(() => {
+            req.session.userId = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: "You are Logged in!!!"});
+
+    });
+});
+
+});
+
+
+//logout
+
+router.post("/logout", (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }else{
+        res.status(404).end();
+    }
+});
+
+router.delete("/id:", (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "No user found with this ID" });
+            return;
+        }
+        res.json(dbUserData);
+    })
+.catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+    
+    });
+
+});
+
+module.exports = router;
