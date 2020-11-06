4866ededd9af9033c151bd04592fd105647b68de
marioviana07 <mario_viana_2005@yahoo.com>
2020-11-06 14:10:11 -0800

    Create DB

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,8 +2,7 @@ const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 
 // POST /api/users
-router.post('/', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+router.post("/", (req, res) => {
     User.create({
             username: req.body.username,
             password: req.body.password,
